6adfeeb7fd8b28cd789b5632fefe07cbe9ec69c2
pittcr3w <pittcrew@ksu.edu>
2017-04-07 17:14:00 -0500

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -2,12 +2,21 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', '/projects/');
 xhr.send(null);
 
-xhr.onreadystatechange = function (){
-  var DONE = 4;
-  var OK = 200;
+xhr.onreadystatechange = function() {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
-      
+      console.log(xhr.responseText); // 'This is the returned text.'
+      var projects = JSON.parse(xhr.responseText);
+      projects.forEach(function(project){
+        var name - document.createElement('a');
+        name.innerHtml = project.name;
+        name.href = "/projects/" + project.id;
+        document.body.appendChild(name);
+      });
+    } else {
+      console.log('Error: ' + xhr.status); // An error occurred during the request.
     }
   }
 }
---
+++ src/resource/project.js
@@ -0,0 +1,110 @@
+"use strict"
+/** @module project
+ * A RESTful resource representing a software project
+*/
+
+module.export = {
+  list: list,
+  create: create,
+  read: read,
+  update: update,
+  destroy: destroy
+}
+
+function list(req, res, db){
+  db.all("SELECT * FROM projects". [], function(err, projects) {
+    if (err){
+      console.console.error(err);
+      res.statusCode = 500;
+      res.end("Server Error");
+    }
+    res.setHeader("Content-Type", "text/json");
+    res.end(JSON.stringify(projects));
+  });
+}
+
+
+function create(req, res, db) {
+    var body = "";
+
+    req.on("error", function(err){
+      console.error(err);
+      res.statusCode = 500;
+      res.end("server error");
+    });
+    req.on("data", function(data){
+      body += data;
+    });
+
+    req.on("end", function(){
+      var project = JSON.parse(body);
+      db.run("INSERT INTO projects (name, description, version, repository, license)"
+      [project.name, project.description, project.version, project.repository, project.license]
+      function(err){
+        if (err){
+          console.error(err);
+          res.statusCode = 500;
+          res.end("Could not insert project into database");
+          return;
+        }
+        res.statusCode = 200;
+        res.end();
+      }
+    );
+    });
+}
+
+function read(req, res, db) {
+  var id = req.params.id;
+  db.get("SELECT 8 FROM projects WHERE id=?", [id], function(err, project){
+    if (err){
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Server error");
+      return;
+    }
+    if(!project) {
+      res.statusCode = 404;
+      res.end("Project not found");
+      return;
+    }
+    res.setHeader("Content-Type", "text/json");
+    res.end(JSON.stringify(project));
+  });
+}
+
+function update(req, res, db){
+  var id = req.params.id;
+
+  req.on("end", function(){
+    var project = JSON.parse(body);
+    db.run("UPDATE projects SET name = ?, description = ?, version = ?, repository = ?, license = ? WHERE id = ?)"
+    [project.name, project.description, project.version, project.repository, project.license, project.id]
+    function(err){
+      if (err){
+        console.error(err);
+        res.statusCode = 500;
+        res.end("Could not update project into database");
+        return;
+      }
+      res.statusCode = 200;
+      res.end();
+    }
+  );
+  });
+
+}
+
+
+function destroy(req, res, db){
+  var id = req.params.id;
+  db.run("DELETE FROM projects WHERE id=?", [id], function(err){
+    if (err){
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Server error");
+    }
+    res.statusCode = 200;
+    res.end();
+  });
+}
